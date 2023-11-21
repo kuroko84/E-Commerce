dec6fded098a36fa0b6bcf77498fbc74c3efac5c
kuroko84 <ttphong071016@gmail.com>
2023-11-21 16:26:29 +0700

    working on shop page

---
+++ src/Components/Items/Item.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Item.css'
+
+const Item = (props) => {
+  return (
+    <div className='item'>
+        <img src={props.image} alt="" />
+        <p>{props.name}</p>
+        <div className="item-prices">
+            <div className="item-price-new">
+                <p>${props.new_price}</p>
+            </div>
+            <div className="item-price-old">
+                <p>${props.old_price}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Item
