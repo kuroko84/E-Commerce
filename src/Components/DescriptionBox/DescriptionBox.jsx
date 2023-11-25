04b1611a564c261b47e055ac33dcf0a6b7796740
kuroko84 <ttphong071016@gmail.com>
2023-11-25 19:22:35 +0700

    add description

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -3,7 +3,32 @@ import './DescriptionBox.css'
 
 const DescriptionBox = () => {
   return (
-    <div className='descriptionbox'></div>
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+          Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+          Reviews (122)
+        </div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Nihil libero rerum eaque 
+          delectus sit voluptatum obcaecati illo 
+          autem deleniti, aut facere doloremque, 
+          vel consequatur tempore similique 
+          veniam cumque modi dicta!
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur 
+          adipisicing elit. Nihil libero rerum eaque 
+          delectus sit voluptatum obcaecati illo 
+          autem deleniti, aut facere doloremque, 
+          vel consequatur tempore similique 
+          veniam cumque modi dicta!
+        </p>
+      </div>
+    </div>
   )
 }
 
